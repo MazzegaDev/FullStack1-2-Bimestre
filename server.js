c1105a36ef32eb50fb972a8d4485c521a3056456
Mazzega_Dev <=>
2025-11-10 10:28:32 -0300

    first commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const categoriaRoute = require('./routes/categoriaRoute');
 const usuarioRoute = require("./routes/usuarioRoute");
 const perfilRoute = require('./routes/perfilRoute');
 const loginRoute = require("./routes/loginRoute");
+const pedidoRoute = require("./routes/pedidosRoute");
 const cookieParser = require("cookie-parser");
 const AuthMiddleware = require('./middlewares/authMiddleware');
 const app = express();
@@ -30,7 +31,7 @@ app.use(cookieParser());
 app.use("/login", loginRoute);
 app.use('/', homeRoute);
 app.use('/produto', produtoRoute);
-
+app.use("/pedido", pedidoRoute);
 let auth = new AuthMiddleware();
 
 app.use(auth.verificarUsuarioLogado);
