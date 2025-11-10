28063c69440a4de98a73a0a9ac98b06ce1885e67
Mazzega_Dev <=>
2025-11-10 19:26:03 -0300

    #

--- controllers/pedidosController.js
+++ controllers/pedidosController.js
@@ -2,54 +2,82 @@ const PedidoItemModel = require("../models/pedidoItemModel");
 const PedidoModel = require("../models/pedidoModel");
 const ProdutoModel = require("../models/produtoModel");
 
-
-
 class PedidosController {
-
-
     async gravar(req, res) {
         console.log(req.body);
         let ok = false;
         let msg = "";
 
         //processo de grvação do pedido
-        if(req.body.length > 0) {
+        //Verfica se o carrinho enviou algum pedido
+        if (req.body.length > 0) {
             let itensPedido = req.body;
             let pedidoModel = new PedidoModel();
+            //Retorna o ID do pedido gerado
             let pedidoId = await pedidoModel.gravar();
-            if(pedidoId > 0) {
+            if (pedidoId > 0) {
                 let produtoModel = new ProdutoModel();
-                for(let i = 0; i < itensPedido.length; i++) {
+                //Itera sobre a quantidade de itens do pedido
+                for (let i = 0; i < itensPedido.length; i++) {
+                    //Pega o id do produto no itensPedido
                     let produtoId = itensPedido[i].id;
-                    let produtoEncontrado = await produtoModel.buscarProduto(produtoId);
-                    if(produtoEncontrado != null) {
-                        let itemPedidoModel = new PedidoItemModel();
-                        itemPedidoModel.produtoId = produtoId;
-                        itemPedidoModel.pedidoId = pedidoId;
-                        itemPedidoModel.pedidoItemValor = produtoEncontrado.produtoPreco;
-                        itemPedidoModel.pedidoItemQuantidade = itensPedido[i].quantidade;
-                        itemPedidoModel.pedidoItemValorTotal = produtoEncontrado.produtoPreco * itensPedido[i].quantidade;
+                    //Procura se o produto existe
+                    let produtoEncontrado = await produtoModel.buscarProduto(
+                        produtoId
+                    );
 
-                        await itemPedidoModel.gravar();
+                    if (produtoEncontrado != null) {
+                        //Verifica o estoque
+                        let quantidadeEstoque =
+                            produtoEncontrado.produtoQuantidade;
+                        if (itensPedido[i].quantidade <= quantidadeEstoque) {
+                            //itensPedido = oque queremos comprar, produto que veio do carrinho
+                            let itemPedidoModel = new PedidoItemModel();
+                            itemPedidoModel.produtoId = produtoId; //Id do produto que veio do carrinho
+                            itemPedidoModel.pedidoId = pedidoId; // Id do pedido gerado - pedidoId = await pedidoModel.gravar()lastinserted
+                            itemPedidoModel.pedidoItemValor =
+                                produtoEncontrado.produtoPreco;
+                            itemPedidoModel.pedidoItemQuantidade =
+                                itensPedido[i].quantidade;
+                            itemPedidoModel.pedidoItemValorTotal =
+                                produtoEncontrado.produtoPreco *
+                                itensPedido[i].quantidade;
 
-                        
+                            //Construa também a baixa de estoque e o valor total, que deve ser feita após a gravação dos itens do pedido
+                            
+                            let estoqueProduto =
+                                produtoEncontrado.produtoQuantidade;
+                            let estoqueBaixa =
+                                estoqueProduto -
+                                itemPedidoModel.pedidoItemQuantidade;
+                            let prdModel = new ProdutoModel();
+                            let prdId = produtoEncontrado.produtoId;
+                            if ( await prdModel.baixaEstoque(prdId, estoqueBaixa)) {
+                                await itemPedidoModel.gravar();
+                                
+                            } else {
+                                ok = false;
+                                msg = `Não foi possivel dar baixa no estoque.`;
+                            }
+                        } else {
+                            ok = false;
+                            msg = `Quantidade em estoque insuficiente para o produto ${produtoEncontrado.produtoNome}`;
+                        }
                     }
                 }
 
                 ok = true;
                 msg = "Pedido gravado com sucesso!";
-            }
-            else {
+            } else {
                 ok = false;
                 msg = "Erro ao gerar pedido!";
             }
-        }
-        else {
+        } else {
             ok = false;
             msg = "Não há produtos no carrinho!";
         }
 
-        res.send({ok: ok, msg: msg});
+        res.send({ ok: ok, msg: msg });
     }
 }
 
--- models/produtoModel.js
+++ models/produtoModel.js
@@ -1,9 +1,8 @@
-const Database = require('../db/database');
+const Database = require("../db/database");
 const fs = require("fs");
 
 const conexao = new Database();
 class ProdutoModel {
-
     #produtoId;
     #produtoCodigo;
     #produtoNome;
@@ -15,24 +14,83 @@ class ProdutoModel {
     #marcaId;
     #marcaNome;
 
-    get produtoId() { return this.#produtoId; } set produtoId(produtoId) {this.#produtoId = produtoId;}
-    get produtoCodigo() { return this.#produtoCodigo; } set produtoCodigo(produtoCodigo) {this.#produtoCodigo = produtoCodigo;}
-    get produtoNome() { return this.#produtoNome; } set produtoNome(produtoNome) {this.#produtoNome = produtoNome;}
-    get produtoQuantidade() { return this.#produtoQuantidade; } set produtoQuantidade(produtoQuantidade) {this.#produtoQuantidade = produtoQuantidade;}
-    get categoriaId() { return this.#categoriaId; } set categoriaId(categoriaId) {this.#categoriaId = categoriaId;}
-    get categoriaNome() { return this.#categoriaNome; } set categoriaNome(categoriaNome) {this.#categoriaNome = categoriaNome;}
-    get marcaId() { return this.#marcaId; } set marcaId(marcaId) {this.#marcaId = marcaId;}
-    get marcaNome() { return this.#marcaNome; } set marcaNome(marcaNome) {this.#marcaNome = marcaNome;}
-    get produtoImagem() { return this.#produtoImagem; } set produtoImagem(produtoImagem) {this.#produtoImagem = produtoImagem;}
-    get produtoPreco() { return this.#produtoPreco; } set produtoPreco(produtoPreco) {this.#produtoPreco = produtoPreco;}
+    get produtoId() {
+        return this.#produtoId;
+    }
+    set produtoId(produtoId) {
+        this.#produtoId = produtoId;
+    }
+    get produtoCodigo() {
+        return this.#produtoCodigo;
+    }
+    set produtoCodigo(produtoCodigo) {
+        this.#produtoCodigo = produtoCodigo;
+    }
+    get produtoNome() {
+        return this.#produtoNome;
+    }
+    set produtoNome(produtoNome) {
+        this.#produtoNome = produtoNome;
+    }
+    get produtoQuantidade() {
+        return this.#produtoQuantidade;
+    }
+    set produtoQuantidade(produtoQuantidade) {
+        this.#produtoQuantidade = produtoQuantidade;
+    }
+    get categoriaId() {
+        return this.#categoriaId;
+    }
+    set categoriaId(categoriaId) {
+        this.#categoriaId = categoriaId;
+    }
+    get categoriaNome() {
+        return this.#categoriaNome;
+    }
+    set categoriaNome(categoriaNome) {
+        this.#categoriaNome = categoriaNome;
+    }
+    get marcaId() {
+        return this.#marcaId;
+    }
+    set marcaId(marcaId) {
+        this.#marcaId = marcaId;
+    }
+    get marcaNome() {
+        return this.#marcaNome;
+    }
+    set marcaNome(marcaNome) {
+        this.#marcaNome = marcaNome;
+    }
+    get produtoImagem() {
+        return this.#produtoImagem;
+    }
+    set produtoImagem(produtoImagem) {
+        this.#produtoImagem = produtoImagem;
+    }
+    get produtoPreco() {
+        return this.#produtoPreco;
+    }
+    set produtoPreco(produtoPreco) {
+        this.#produtoPreco = produtoPreco;
+    }
 
-
-
-    constructor(produtoId, produtoCodigo, produtoNome, produtoQuantidade, categoriaId, marcaId, categoriaNome, marcaNome, produtoImagem, produtoPreco) {
-        this.#produtoId = produtoId
-        this.#produtoCodigo = produtoCodigo
-        this.#produtoNome = produtoNome
-        this.#produtoQuantidade = produtoQuantidade
+    constructor(
+        produtoId,
+        produtoCodigo,
+        produtoNome,
+        produtoQuantidade,
+        categoriaId,
+        marcaId,
+        categoriaNome,
+        marcaNome,
+        produtoImagem,
+        produtoPreco
+    ) {
+        this.#produtoId = produtoId;
+        this.#produtoCodigo = produtoCodigo;
+        this.#produtoNome = produtoNome;
+        this.#produtoQuantidade = produtoQuantidade;
         this.#categoriaId = categoriaId;
         this.#categoriaNome = categoriaNome;
         this.#marcaId = marcaId;
@@ -41,8 +99,8 @@ class ProdutoModel {
         this.#produtoPreco = produtoPreco;
     }
 
-    async excluir(codigo){
-        let sql = "delete from tb_produto where prd_id = ?"
+    async excluir(codigo) {
+        let sql = "delete from tb_produto where prd_id = ?";
         let valores = [codigo];
 
         var result = await conexao.ExecutaComandoNonQuery(sql, valores);
@@ -51,67 +109,89 @@ class ProdutoModel {
     }
 
     async gravar() {
-        if(this.#produtoId == 0){
-            let sql = "insert into tb_produto (prd_cod, prd_nome, prd_quantidade, cat_id, mar_id, prd_imagem, prd_preco) values (?, ?, ?, ?, ?, ?, ?)";
+        if (this.#produtoId == 0) {
+            let sql =
+                "insert into tb_produto (prd_cod, prd_nome, prd_quantidade, cat_id, mar_id, prd_imagem, prd_preco) values (?, ?, ?, ?, ?, ?, ?)";
 
-            let valores = [this.#produtoCodigo, this.#produtoNome, this.#produtoQuantidade, this.#categoriaId, this.#marcaId, this.#produtoImagem, this.#produtoPreco];
+            let valores = [
+                this.#produtoCodigo,
+                this.#produtoNome,
+                this.#produtoQuantidade,
+                this.#categoriaId,
+                this.#marcaId,
+                this.#produtoImagem,
+                this.#produtoPreco,
+            ];
 
             return await conexao.ExecutaComandoNonQuery(sql, valores);
-        }
-        else{
+        } else {
             //alterar
-            let sql = "update tb_produto set prd_cod = ?, prd_nome =?, prd_quantidade= ?, cat_id = ?, mar_id = ?, prd_imagem = ?, prd_preco = ? where prd_id = ?";
+            let sql =
+                "update tb_produto set prd_cod = ?, prd_nome =?, prd_quantidade= ?, cat_id = ?, mar_id = ?, prd_imagem = ?, prd_preco = ? where prd_id = ?";
 
-            let valores = [this.#produtoCodigo, this.#produtoNome, 
-                this.#produtoQuantidade, 
-                this.#categoriaId, 
-                this.#marcaId, this.#produtoImagem, this.produtoPreco, this.#produtoId];
+            let valores = [
+                this.#produtoCodigo,
+                this.#produtoNome,
+                this.#produtoQuantidade,
+                this.#categoriaId,
+                this.#marcaId,
+                this.#produtoImagem,
+                this.produtoPreco,
+                this.#produtoId,
+            ];
 
-            return await conexao.ExecutaComandoNonQuery(sql, valores) > 0;
+            return (await conexao.ExecutaComandoNonQuery(sql, valores)) > 0;
         }
     }
 
-    async buscarProduto(id){
-        let sql = 'select * from tb_produto where prd_id = ? order by prd_id';
+    async buscarProduto(id) {
+        let sql = "select * from tb_produto where prd_id = ? order by prd_id";
         let valores = [id];
         var rows = await conexao.ExecutaComando(sql, valores);
 
         let produto = null;
 
-        if(rows.length > 0){
+        if (rows.length > 0) {
             var row = rows[0];
 
             //armazenamento em diretório
             let imagem = "";
-            if(row["prd_imagem"] != null && 
-                fs.existsSync(global.CAMINHO_IMG_ABS + row["prd_imagem"])) {
+            if (
+                row["prd_imagem"] != null &&
+                fs.existsSync(global.CAMINHO_IMG_ABS + row["prd_imagem"])
+            ) {
                 imagem = global.CAMINHO_IMG + row["prd_imagem"];
-            }
-            else {
+            } else {
                 imagem = global.CAMINHO_IMG + "produto-sem-imagem.webp";
             }
-                    
-            
-            produto = new ProdutoModel(row['prd_id'], 
-            row['prd_cod'], row['prd_nome'], row['prd_quantidade'], 
-            row['cat_id'], row['mar_id'], "", "", imagem, row["prd_preco"]);
 
+            produto = new ProdutoModel(
+                row["prd_id"],
+                row["prd_cod"],
+                row["prd_nome"],
+                row["prd_quantidade"],
+                row["cat_id"],
+                row["mar_id"],
+                "",
+                "",
+                imagem,
+                row["prd_preco"]
+            );
         }
 
         return produto;
     }
 
     async listarProdutos() {
+        let sql =
+            "select * from tb_produto p inner join tb_categoria c on p.cat_id = c.cat_id inner join tb_marca m on p.mar_id = m.mar_id order by prd_id";
 
-        let sql = 'select * from tb_produto p inner join tb_categoria c on p.cat_id = c.cat_id inner join tb_marca m on p.mar_id = m.mar_id order by prd_id';
-        
         var rows = await conexao.ExecutaComando(sql);
 
         let listaRetorno = [];
 
-        if(rows.length > 0){
-            for(let i=0; i<rows.length; i++){
-
+        if (rows.length > 0) {
+            for (let i = 0; i < rows.length; i++) {
                 var row = rows[i];
 
                 //converter binário da imagem em base64 (armazenamento em banco)
@@ -122,23 +202,41 @@ class ProdutoModel {
 
                 //armazenamento em diretório
                 let imagem = "";
-                if(row["prd_imagem"] != null 
-                    && 
-                fs.existsSync(global.CAMINHO_IMG_ABS + row["prd_imagem"])) {
+                if (
+                    row["prd_imagem"] != null &&
+                    fs.existsSync(global.CAMINHO_IMG_ABS + row["prd_imagem"])
+                ) {
                     imagem = global.CAMINHO_IMG + row["prd_imagem"];
-                }
-                else {
+                } else {
                     imagem = global.CAMINHO_IMG + "produto-sem-imagem.webp";
                 }
-                    
 
-                listaRetorno.push(new ProdutoModel(row['prd_id'], 
-                row['prd_cod'], row['prd_nome'], row['prd_quantidade'], 
-                row['cat_id'], row['mar_id'], row['cat_nome'], row['mar_nome'], imagem, row["prd_preco"]));
+                listaRetorno.push(
+                    new ProdutoModel(
+                        row["prd_id"],
+                        row["prd_cod"],
+                        row["prd_nome"],
+                        row["prd_quantidade"],
+                        row["cat_id"],
+                        row["mar_id"],
+                        row["cat_nome"],
+                        row["mar_nome"],
+                        imagem,
+                        row["prd_preco"]
+                    )
+                );
             }
         }
 
         return listaRetorno;
+    }
+
+    async baixaEstoque(prd_id, nBaixa) {
+        const sql = "update tb_produto set prd_quantidade = ? where prd_id = ?";
+        const values = [prd_id, nBaixa];
+
+        const result = await conexao.ExecutaComandoNonQuery(sql, values);
+        return result;
     }
 
     toJSON() {
@@ -146,10 +244,9 @@ class ProdutoModel {
             id: this.#produtoId,
             nome: this.#produtoNome,
             preco: this.#produtoPreco,
-            imagem: this.#produtoImagem
-        }
+            imagem: this.#produtoImagem,
+        };
     }
-
 }
 
 module.exports = ProdutoModel;
